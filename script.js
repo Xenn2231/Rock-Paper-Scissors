4ad6e206a601f4a0230cbfea63e56ef501e7e00b
Ruslan <183654647+Xenn2231@users.noreply.github.com>
2025-01-18 12:01:15 +0200

    Added functions for human and computer choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const items = ['Rock', 'Paper', 'Scissors']
+
+
+
+function getComputerChoice() {
+    const randomIndex =  Math.floor(Math.random() * 3);
+
+    return items[randomIndex];
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt('Enter "Rock", "Paper" or "Scissors"')
+
+    if (items.includes(humanChoice)) {
+        return humanChoice;
+    }
+}
