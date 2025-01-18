a506c5a33a32ae97fb5e7ba5c59001c243ecf0e3
Ruslan <183654647+Xenn2231@users.noreply.github.com>
2025-01-18 12:28:58 +0200

    Added score system and system to play a round

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const items = ['Rock', 'Paper', 'Scissors']
+const items = ['rock', 'paper', 'scissors']
 
-
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const randomIndex =  Math.floor(Math.random() * 3);
@@ -11,9 +12,46 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    const humanChoice = prompt('Enter "Rock", "Paper" or "Scissors"')
+    const humanChoice = prompt('Enter "Rock", "Paper" or "Scissors"').toLowerCase()
 
     if (items.includes(humanChoice)) {
         return humanChoice;
+    } else {
+        alert('Opps, please enter valid text')
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+
+    if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        humanScore++;
+        console.log(
+           `Human won, ${humanChoice} beats ${computerChoice}. 
+            Human score: ${humanScore}. 
+            Computer score ${computerScore}.`
+        )
+    } else if (humanChoice === computerChoice) {
+        console.log(
+           `Draw. 
+            Human score: ${humanScore}. 
+            Computer score ${computerScore}.`
+        )
+    } else{
+        computerScore++;
+        console.log(
+            `Computer won, ${computerChoice} beats ${humanChoice}. 
+             Human score: ${humanScore}. 
+             Computer score ${computerScore}.`
+         )
+    }
+
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
