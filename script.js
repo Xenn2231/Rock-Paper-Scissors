289a7add108858d6a730478d0e6320e5103ec66c
Ruslan <183654647+Xenn2231@users.noreply.github.com>
2025-01-18 14:26:04 +0200

    UI added

--- script.js
+++ script.js
@@ -1,72 +1,93 @@
 "use strict";
 
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const result = document.getElementById('result');
+const reload = document.getElementById('reload');
+
+const maxRounds = 5;
+let humanScore = 0;
+let computerScore = 0;
+
 const items = ['rock', 'paper', 'scissors']
 
 function getComputerChoice() {
     const randomIndex =  Math.floor(Math.random() * 3);
-
     return items[randomIndex];
 }
 
-function getHumanChoice() {
-    const humanChoice = prompt('Enter "Rock", "Paper" or "Scissors"').toLowerCase()
-
-    if (items.includes(humanChoice)) {
-        return humanChoice;
-    } else {
-        alert('Opps, please enter valid text')
-        return getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        humanScore++;
+        result.innerText = 
+           `Human won, ${humanChoice} beats ${computerChoice}. 
+            Human score: ${humanScore}. 
+            Computer score ${computerScore}.`
+    } else if (humanChoice === computerChoice) {
+        result.innerText = 
+           `Draw. 
+            Human score: ${humanScore}. 
+            Computer score ${computerScore}.`
+    } else{
+        computerScore++;
+        result.innerText = 
+            `Computer won, ${computerChoice} beats ${humanChoice}. 
+             Human score: ${humanScore}. 
+             Computer score ${computerScore}.`
     }
 }
 
-function playGame() {
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-
-        if (
-            (humanChoice === 'rock' && computerChoice === 'scissors') ||
-            (humanChoice === 'paper' && computerChoice === 'rock') ||
-            (humanChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-            humanScore++;
-            console.log(
-               `Human won, ${humanChoice} beats ${computerChoice}. 
-                Human score: ${humanScore}. 
-                Computer score ${computerScore}.`
-            )
-        } else if (humanChoice === computerChoice) {
-            console.log(
-               `Draw. 
-                Human score: ${humanScore}. 
-                Computer score ${computerScore}.`
-            )
-        } else{
-            computerScore++;
-            console.log(
-                `Computer won, ${computerChoice} beats ${humanChoice}. 
-                 Human score: ${humanScore}. 
-                 Computer score ${computerScore}.`
-             )
-        }
-    
-    }
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection) 
-    }
-
-    if (humanScore > computerScore) {
-        alert(`Human won the game! Final score: Human-${humanScore}   Computer-${computerScore}`)
-    } else if (humanScore < computerScore) {
-        alert(`Computer won the game! Final score: Human-${humanScore}   Computer-${computerScore}`)
-    } else {
-        alert(`It is a tie! Final score: Human-${humanScore}   Computer-${computerScore}`)
+function checkGameOver() {
+    if (humanScore === maxRounds) {
+        result.innerText = `Human won. Score: Human-${humanScore}, Computer-${computerScore}`
+        disableButtons()
+    } else if (computerScore === maxRounds) {
+        result.innerText = `Computer won. Score: Human-${humanScore}, Computer-${computerScore}.`
+        disableButtons()
+    } else if (humanScore === computerScore && computerScore >= 5 || humanScore >= 5) {
+        result.innerText = `It is a tie. Score: Human-${humanScore}, Computer-${computerScore}.`
+        disableButtons()
     }
 }
 
-playGame()
+function disableButtons() {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+    reload.style.display = 'inline-block';
+}
+
+function reloadGame() {
+    result.innerText = ' ';
+    reload.style.display = 'none';
+    humanScore = 0;
+    computerScore = 0;
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+}
+
+rock.addEventListener('click', () => {
+    const computerSelection = getComputerChoice()
+    playRound('rock', computerSelection)
+    checkGameOver()
+})
+
+paper.addEventListener('click', () => {
+    const computerSelection = getComputerChoice()
+    playRound('paper', computerSelection)
+    checkGameOver()
+})
+
+scissors.addEventListener('click', () => {
+    const computerSelection = getComputerChoice()
+    playRound('scissors', computerSelection)
+    checkGameOver()
+})
+
+reload.addEventListener('click', reloadGame)
