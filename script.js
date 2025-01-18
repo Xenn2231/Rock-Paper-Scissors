a334d1b2fcc8e655d9e2426ae21a95d0d844b0ab
Ruslan <183654647+Xenn2231@users.noreply.github.com>
2025-01-18 12:43:37 +0200

    Added ability to play a full game in a console

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 
 const items = ['rock', 'paper', 'scissors']
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const randomIndex =  Math.floor(Math.random() * 3);
 
@@ -18,40 +15,58 @@ function getHumanChoice() {
         return humanChoice;
     } else {
         alert('Opps, please enter valid text')
+        return getHumanChoice();
     }
 }
 
-function playRound(humanChoice, computerChoice) {
+function playGame() {
 
-    if (
-        (humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'paper' && computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-        humanScore++;
-        console.log(
-           `Human won, ${humanChoice} beats ${computerChoice}. 
-            Human score: ${humanScore}. 
-            Computer score ${computerScore}.`
-        )
-    } else if (humanChoice === computerChoice) {
-        console.log(
-           `Draw. 
-            Human score: ${humanScore}. 
-            Computer score ${computerScore}.`
-        )
-    } else{
-        computerScore++;
-        console.log(
-            `Computer won, ${computerChoice} beats ${humanChoice}. 
-             Human score: ${humanScore}. 
-             Computer score ${computerScore}.`
-         )
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+
+        if (
+            (humanChoice === 'rock' && computerChoice === 'scissors') ||
+            (humanChoice === 'paper' && computerChoice === 'rock') ||
+            (humanChoice === 'scissors' && computerChoice === 'paper')
+        ) {
+            humanScore++;
+            console.log(
+               `Human won, ${humanChoice} beats ${computerChoice}. 
+                Human score: ${humanScore}. 
+                Computer score ${computerScore}.`
+            )
+        } else if (humanChoice === computerChoice) {
+            console.log(
+               `Draw. 
+                Human score: ${humanScore}. 
+                Computer score ${computerScore}.`
+            )
+        } else{
+            computerScore++;
+            console.log(
+                `Computer won, ${computerChoice} beats ${humanChoice}. 
+                 Human score: ${humanScore}. 
+                 Computer score ${computerScore}.`
+             )
+        }
+    
     }
 
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+        playRound(humanSelection, computerSelection) 
+    }
+
+    if (humanScore > computerScore) {
+        alert(`Human won the game! Final score: Human-${humanScore}   Computer-${computerScore}`)
+    } else if (humanScore < computerScore) {
+        alert(`Computer won the game! Final score: Human-${humanScore}   Computer-${computerScore}`)
+    } else {
+        alert(`It is a tie! Final score: Human-${humanScore}   Computer-${computerScore}`)
+    }
 }
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
-
-playRound(humanSelection, computerSelection)
+playGame()
